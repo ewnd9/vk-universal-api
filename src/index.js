abdfd7c0ad071be6d7c678b0c3e23a0005319f35
ewnd9 <ewndnine@gmail.com>
2016-01-30 05:33:34 +0800

    move setToken to universal-api module

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import UniversalApi from 'universal-api';
 
-function VkUniversalApi() {
-  UniversalApi.call(this, {
-    baseUrl: 'https://api.vk.com/method/',
-    jsonp: true,
-    query: () => ({ access_token: this.token }),
-    transformResponse: response => {
-      if (response.response) {
-        return response.response;
-      } else {
-        throw response.error;
-      }
+export default new UniversalApi({
+  baseUrl: 'https://api.vk.com/method/',
+  jsonp: true,
+  query: function() {
+    return {
+      access_token: this.token
+    };
+  },
+  transformResponse: response => {
+    if (response.response) {
+      return response.response;
+    } else {
+      throw response.error;
     }
-  });
-};
-
-VkUniversalApi.prototype = Object.create(UniversalApi.prototype);
-
-VkUniversalApi.prototype.setToken = function(token) {
-  this.token = token;
-};
-
-export default new VkUniversalApi();
+  }
+});
